d789ed41405ba78fdb2d67153106eab862ceeda7
Fabmach218 <60011748+Fabmach218@users.noreply.github.com>
2022-02-22 23:36:00 -0500

    Adding comments to understand better

--- classes.py
+++ classes.py
@@ -2,15 +2,18 @@ import random
 
 class Student:
 
+    #Constructor (use self before parameters)
     def __init__(self, fname, lname):
 
-        self.idnum = Student.gen_id()
+        self.idnum = Student.gen_id() #Calling static method (class goes always before)
         self.fname = fname
         self.lname = lname
 
+    #Instance method
     def concat_name(self):
         return self.fname + ' ' + self.lname
 
+    #Static method
     def gen_id():
 
         idnum = str(random.randrange(1,10**8))
@@ -23,12 +26,12 @@ class Student:
 
 list = []
 
-list.append(Student('Fabio', 'Marangon'))
+list.append(Student('Fabio', 'Marangon')) ##Self parameter in constructor is not put here
 list.append(Student('Juan', 'Perez'))
 list.append(Student('Fernando', 'Sanchez'))
 
 print('ID\tFull Name')
 
 for item in list:
-    print(item.idnum + '\t' + item.concat_name())
+    print(item.idnum + '\t' + item.concat_name()) ##Calling instance method (no self)
 
